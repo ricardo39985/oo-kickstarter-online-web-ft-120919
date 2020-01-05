4af83a1d9538cd9b20f9cf3245821e978b7cb713
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-05 16:41:55 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+require "pry"
+class Backer
+  attr_reader :backed_projects, :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+    @@all << self
+  end
+  def back_project(project)
+    project.add_backer(self)
+    @backed_projects << project
+    # binding.pry
+  end
+end
